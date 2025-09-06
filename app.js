76c25f8853ff063a939a26de03de876a9db42e67
Viji Ramu <vijiramu@gmail.com>
2025-09-06 14:41:27 -0500

    Updated files

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const stringUtil = require('./stringUtil');
 // Test capitalize
 console.log("Capitalize first letter of the string:");
 console.log(stringUtil.capitalize("launch Code"));   // Output: Hello
-/*console.log(stringUtils.capitalize(""));        // Output: (empty string)
-console.log(stringUtils.capitalize(undefined)); // Output: (empty string)
+console.log(stringUtil.capitalize(""));        // Output: (empty string)
+console.log(stringUtil.capitalize(undefined)); // Output: (empty string)
 
 // Test reverse
 console.log("\nReverse given string:");
-console.log(stringUtils.reverse("JavaScript")); // Output: tpircSavaJ
-console.log(stringUtils.reverse("")); // Output: (empty string)
-console.log(stringUtils.reverse("Inheretance")); // Output: ecnaterehnI
-console.log(stringUtils.reverse(undefined)); // Output: (empty string)
+console.log(stringUtil.reverse("JavaScript")); // Output: tpircSavaJ
+console.log(stringUtil.reverse("")); // Output: (empty string)
+console.log(stringUtil.reverse("Inheretance")); // Output: ecnaterehnI
+console.log(stringUtil.reverse(undefined)); // Output: (empty string)
 
 // Test contains
 console.log("\ncheck if the string Contains another string:");
-console.log(stringUtils.contains("Launch code", "code")); // Output: true
-console.log(stringUtils.contains("hello world", "js"));    // Output: false
-console.log(stringUtils.contains("", "a"));                // Output: false
-console.log(stringUtils.contains("test", ""));             // Output: true
-*/
+console.log(stringUtil.contains("Launch code", "code")); // Output: true
+console.log(stringUtil.contains("hello world", "js"));    // Output: false
+console.log(stringUtil.contains("", "a"));                // Output: false
+console.log(stringUtil.contains("test", ""));             // Output: true
+
